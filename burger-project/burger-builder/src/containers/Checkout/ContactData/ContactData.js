05ce872a1e80fd4bbbd0b38936050fd19d663add
Kim Hojeong <hojeong0713@gmail.com>
2021-03-18 17:11:26 +0900

    Fix connecting ContactData container and Actions

--- burger-project/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-project/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.css'
 import axios from '../../../axios-order'
 import Input from '../../../components/UI/Input/Input'
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../../store/actions/index'
 
 class ContactData extends Component {
 	state = {
@@ -96,7 +98,6 @@ class ContactData extends Component {
 
 	orderHandler = (event) => {
 		event.preventDefault()
-		this.setState({ loading: true })
 
 		const formData = {}
 		for (let formElementIndetifier in this.state.orderForm) {
@@ -108,6 +109,8 @@ class ContactData extends Component {
 			price: this.props.price,
 			orderData: formData,
 		}
+
+		this.props.onOrderBurger(order)
 	}
 
 	checkValidity(value, rules) {
@@ -204,4 +207,11 @@ const mapStateToProps = (state) => {
 	}
 }
 
-export default connect(mapStateToProps)(ContactData)
+const mapDispatchToProps = dispatch => {
+	return {
+		onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+	}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios))
