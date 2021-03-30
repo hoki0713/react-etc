5e781437c8ca232234d978cbc945fd8e98f03af0
Kim Hojeong <hojeong0713@gmail.com>
2021-03-30 16:31:22 +0900

    Add dotenv

--- burger-project/burger-builder/src/index.js
+++ burger-project/burger-builder/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+import dotenv from 'dotenv'
+
 
 import './index.css'
 import App from './App'
@@ -11,6 +13,8 @@ import reportWebVitals from './reportWebVitals'
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import orderReducer from './store/reducers/order'
 import authReducer from './store/reducers/auth'
+
+dotenv.config()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
--- burger-project/burger-builder/src/store/actions/auth.js
+++ burger-project/burger-builder/src/store/actions/auth.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
-
-import { FIREBASE_API_KEY } from '../../../.env'
 import * as actionTypes from './actionTypes'
 
 export const authStart = () => {
@@ -32,10 +30,10 @@ export const auth = (email, password, isSignup) => {
       password: password,
       returnSecureToken: true
     }
-    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.FIREBASE_API_KEY}`
 
     if(!isSignup) {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASE_API_KEY}`
     }
 
     axios
