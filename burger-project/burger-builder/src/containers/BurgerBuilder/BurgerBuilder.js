991eb5a7a0042fefeb4268595b5d74f1f7ede487
Kim Hojeong <hojeong0713@gmail.com>
2021-03-31 22:40:40 +0900

    Fix forward unauthenticated user

--- burger-project/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,7 +38,11 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseHandler = () => {
-		this.setState({ purchasing: true })
+		if (this.props.isAuthenticated) {
+			this.setState({ purchasing: true })
+		} else {
+			this.props.history.push('/auth')
+		}
 	}
 
 	purchaseCancelHandler = () => {
@@ -72,6 +76,7 @@ class BurgerBuilder extends Component {
 						price={this.props.price}
 						purchasable={this.updatePurchaseState(this.props.ings)}
 						ordered={this.purchaseHandler}
+						isAuth={this.props.isAuthenticated}
 					/>
 				</Aux>
 			)
@@ -100,7 +105,8 @@ const mapStateToProps = (state) => {
 	return {
 		ings: state.burgerBuilder.ingredients,
 		price: state.burgerBuilder.totalPrice,
-		error: state.burgerBuilder.error
+		error: state.burgerBuilder.error,
+		isAuthenticated: state.auth.token !== null
 	}
 }
 
