9eef61352db54892d7c7d918d4a620bcf17c3841
Kim Hojeong <hojeong0713@gmail.com>
2021-04-07 14:31:52 +0900

    WIP: fix auth action test code

--- burger-project/burger-builder/src/store/actions/auth.test.js
+++ burger-project/burger-builder/src/store/actions/auth.test.js
@@ -12,59 +12,42 @@ const mockStore = configureMockStore(middlewares)
 const mock = new MockAdapter(axios)
 const store = mockStore()
 
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = String(value);
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-};
-
 describe('auth actions', () => {
   beforeEach(() => {
     store.clearActions()
-    global.localStorage = new LocalStorageMock;
   })
 
-  it('dispatch AUTH_SUCCESS for signIn', async () => {
+  it('dispatch AUTH_SUCCESS for signIn', () => {
 
     mock.onPost(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`)
       .reply(201, { 
         response: { 
           data: {
-            expiresIn: 'some-expiresIn',
             idToken: 'some-idToken',
+            email: 'some-email',
+            refreshToken: 'some-refreshToken',
+            expiresIn: 'some-expiresIn',
             localId: 'some-localId',
-            error: 'some-error'
+            registered: true
           }
         }
       })
-
-
-    const expectedActions = [
-      { type: actionTypes.AUTH_START },
-      // { type: actionTypes.AUTH_SUCCESS, authData: { email: 'some-email', password: 'some-password', returnSecureToken: true} }
-    ]
-
-
-
-    await store.dispatch(actions.auth('some-email','some-password', true))
     
-    console.log(store.getActions())
+    const expectedActions = [
+        { type: actionTypes.AUTH_START },
+        { type: actionTypes.AUTH_SUCCESS,
+          idToken: 'some-idToken',
+          userId: 'some-localId' },
+        { type: actionTypes.AUTH_FAIL,
+          error: 'some-error'
+        }
+      ]
+        
+    store.dispatch(actions.auth('some-email','some-password', true))
+    store.dispatch(actions.authSuccess('some-idToken', 'some-localId'))
+    store.dispatch(actions.checkAuthTimeout('some-expiresIn'))
+    store.dispatch(actions.authFail('some-error'))
+
     expect(store.getActions()).toEqual(expectedActions)
   })
 })
