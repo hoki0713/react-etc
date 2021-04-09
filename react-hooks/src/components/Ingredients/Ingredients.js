a2a8114531678ca50eabb9786c03e17fefd0155e
Kim Hojeong <hojeong0713@gmail.com>
2021-04-09 22:06:09 +0900

    Fix: passing state data across component

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import IngredientForm from './IngredientForm'
+import IngredientList from './IngredientList'
 import Search from './Search'
 
 const Ingredients = (props) => {
+  const [ userIngredients, setUserIngredients ] = useState([])
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients(prevIngredients => (
+      [...prevIngredients, 
+        {
+          id : Math.random().toString(), 
+          ...ingredient
+        }
+      ]))
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
       <section>
         <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   )
