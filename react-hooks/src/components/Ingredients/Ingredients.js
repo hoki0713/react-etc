02a6bec0a83d7fb64cbc4d6fef0fa83291e97c20
Kim Hojeong <hojeong0713@gmail.com>
2021-04-12 17:11:46 +0900

    Fix: connect to firebase realtime db

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -8,13 +8,22 @@ const Ingredients = (props) => {
   const [ userIngredients, setUserIngredients ] = useState([])
 
   const addIngredientHandler = (ingredient) => {
-    setUserIngredients(prevIngredients => (
-      [...prevIngredients, 
-        {
-          id : Math.random().toString(), 
-          ...ingredient
-        }
-      ]))
+    fetch('https://react-hooks-update-3b865-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json()
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => (
+        [...prevIngredients, 
+          {
+            id : responseData.name, 
+            ...ingredient
+          }
+        ])
+      )
+    })
   }
 
   const removeIngredientHandler = (igId) => {
