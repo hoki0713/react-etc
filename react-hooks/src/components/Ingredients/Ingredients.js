d5cd6daa6ff8ddc53eb03be76d8a4378864a4475
Kim Hojeong <hojeong0713@gmail.com>
2021-04-13 18:48:13 +0900

    Fix: add loading indicator and error modal

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect, useCallback } from 'react'
 
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
+import ErrorModal from '../UI/ErrorModal'
 import Search from './Search'
 
 const Ingredients = (props) => {
   const [ userIngredients, setUserIngredients ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(false)
+  const [ error, setError ] = useState()
 
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients)
@@ -16,11 +19,13 @@ const Ingredients = (props) => {
   }, [])
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true)
     fetch('https://react-hooks-update-3b865-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
+      setIsLoading(false)
       return response.json()
     }).then(responseData => {
       setUserIngredients(prevIngredients => (
@@ -35,16 +40,26 @@ const Ingredients = (props) => {
   }
 
   const removeIngredientHandler = (igId) => {
+    setIsLoading(true)
     fetch(`https://react-hooks-update-3b865-default-rtdb.firebaseio.com/ingredients/${igId}.json`,{
       method: 'DELETE'
     }).then(response => {
+      setIsLoading(false)
       setUserIngredients(prevIngs => prevIngs.filter(ig => ig.id !== igId))
+    }).catch(error => {
+      setError('Something went wrong!/n', error.message)
     })
+  }
+
+  const clearError = () => {
+    setError(null)
+    setIsLoading(false)
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
