1d679091984e658f354aa590fdbe86468c7decdc
Kim Hojeong <hojeong0713@gmail.com>
2021-04-13 18:25:10 +0900

    Fix clean up useEffect

--- react-hooks/src/components/Ingredients/Search.js
+++ react-hooks/src/components/Ingredients/Search.js
@@ -9,7 +9,7 @@ const Search = React.memo(props => {
   const inputRef = useRef()
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`
         fetch('https://react-hooks-update-3b865-default-rtdb.firebaseio.com/ingredients.json' + query)
@@ -27,6 +27,9 @@ const Search = React.memo(props => {
           })
       }
     }, 500)
+    return () => {
+      clearTimeout(timer)
+    }
   }, [enteredFilter, onLoadIngredients, inputRef])
 
   return (
